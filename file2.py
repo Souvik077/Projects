65a6f87b87baa943bffbbe1f1b38c0871222c347
Souvik <ssmahapatra1530@gmail.com>
2022-12-12 20:56:46 +0530

    adding file2.py

---
+++ file2.py
@@ -0,0 +1,9 @@
+import sys
+def angle_clock(time):
+ hour, minute = time.split(':')
+ hour = int(hour)
+ minute = int(minute)
+ angle = abs(hour%12 * 30 + minute * 0.5 - minute * 6)
+ return min(angle, 360 - angle)
+time = sys.stdin.readline().strip()
+print(angle_clock(time))
